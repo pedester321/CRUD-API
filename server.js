a42c85422aa2816113ecd581a2a0d0edd8e4245f
pedester321 <pedro.castro.95@outlook.com>
2024-07-04 19:43:54 -0300

    Fixed swagger but still needs work

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import { fastify } from 'fastify'
-import swagger from 'fastify-swagger';
-const fastify = Fastify({ logger: true });
+import Fastify from 'fastify';
+import swagger from '@fastify/swagger';
+import swaggerUi from '@fastify/swagger-ui';
 
 import { DatabasePostgres } from './database-postgres.js'
 
 const database = new DatabasePostgres()
-const server = fastify()
+const server = Fastify({logger: true})
 
 // Configuração do Swagger
-fastify.register(swagger, {
+server.register(swagger, {
     routePrefix: '/documentation',
     swagger: {
       info: {
-        title: 'API Example',
-        description: 'API Example with Fastify and Swagger',
+        title: 'API GroupStore',
+        description: 'API do projeto GroupStore',
         version: '1.0.0'
       },
       host: 'https://crud-api-4l21.onrender.com',
@@ -21,8 +21,26 @@ fastify.register(swagger, {
       consumes: ['application/json'],
       produces: ['application/json'],
     },
+    exposeRoute: true,
+  ajv: {
+    customOptions: {
+      strict: false
+    }
+  }
+});
+
+server.register(swaggerUi, {
+    routePrefix: '/docs',
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false,
+    },
+    transformSpecification: (swaggerObject, request, reply) => {
+      return swaggerObject;
+    },
+    transformSpecificationClone: true,
     exposeRoute: true
-  })
+  });
 
 //GET
 server.get('/',{
@@ -38,7 +56,7 @@ server.get('/',{
     }
   }, () =>{
     return 'API GroupStore'
-})
+});
 
 server.get('/products' ,{
     schema: {
@@ -60,7 +78,7 @@ server.get('/products' ,{
             properties: {
               id: { type: 'string', format: 'uuid', description: 'UUID do produto' },
               name: { type: 'string' },
-              price: { type: 'number', example: 9.99, description: 'Preço do produto' },
+              price: { type: 'number', description: 'Preço do produto' },
               description: { type: 'string' }
             }
           }
@@ -73,10 +91,10 @@ server.get('/products' ,{
     const products = database.getProducts(search)
 
     return products
-})
+});
 
 //POST
-server.post('/products',{
+server.post('/products', {
     schema: {
       description: 'Cria um novo produto',
       tags: ['Products'],
@@ -85,13 +103,20 @@ server.post('/products',{
         required: ['name', 'price', 'description'],
         properties: {
           name: { type: 'string' },
-          price: { type: 'number', example: 9.99 },
+          price: { type: 'number' },
           description: { type: 'string' }
         }
       },
       response: {
         201: {
-          description: 'Produto criado com sucesso'
+            description: 'Produto criado com sucesso',
+            type: 'object',
+            properties: {
+              id: { type: 'string', format: 'uuid' },
+              name: { type: 'string' },
+              price: { type: 'number' },
+              description: { type: 'string' }
+            }
         }
       }
     }
@@ -106,7 +131,7 @@ server.post('/products',{
 
     console.log(database.getProducts())
     return reply.status(201).send() //algo foi criado
-})
+});
 
 //PUT
 server.put('/products/:id',{
@@ -125,14 +150,15 @@ server.put('/products/:id',{
         required: ['name', 'price', 'description'],
         properties: {
           name: { type: 'string' },
-          price: { type: 'number', example: 9.99 },
+          price: { type: 'number'},
           description: { type: 'string' }
         }
       },
       response: {
         204: {
-          description: 'Produto atualizado com sucesso (sem conteúdo)'
-        }
+            description: 'Produto atualizado com sucesso (sem conteúdo)',
+            type: 'null'
+          }
       }
     }
   },(request, reply) => {
@@ -146,7 +172,7 @@ server.put('/products/:id',{
     })
 
     return reply.status(204).send //sucesso sem conteudo
-})
+});
 
 //DELETE
 server.delete('/products/:id',{
@@ -162,7 +188,8 @@ server.delete('/products/:id',{
       },
       response: {
         204: {
-          description: 'Produto excluído com sucesso (sem conteúdo)'
+          description: 'Produto excluído com sucesso (sem conteúdo)',
+            type: 'null'
         }
       }
     }
@@ -172,11 +199,11 @@ server.delete('/products/:id',{
     database.deleteProduct(productId)
 
     return reply.status(204).send //sucesso sem conteudo
-})
+});
 
 server.listen(
     {
         host: '0.0.0.0',
         port: process.env.PORT ?? 3333,
     }
-)
+);
